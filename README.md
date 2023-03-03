9df15261b2d30b925eb076a2ebf961751bdf08fd
Angelo Damiani <a.damiani@dpstudio.ch>
2023-03-03 08:24:25 +0100

    init

--- README.md
+++ README.md
@@ -53,5 +53,3 @@ Apply database updates by running
 
 
 
-# test-magento-custom-attribute
-# test-magento-custom-attribute
--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -1,5 +1,7 @@
 <?php
-namespace Lucky_CustomAttribute\Setup;
+
+namespace Lucky\NewsletterApi\Setup;
+
 use Magento\Customer\Setup\CustomerSetupFactory;
 use Magento\Customer\Model\Customer;
 use Magento\Eav\Model\Entity\Attribute\Set as AttributeSet;
@@ -7,16 +9,23 @@ use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
 class InstallData implements InstallDataInterface
 {
+
     /**
      * @var CustomerSetupFactory
      */
     protected $customerSetupFactory;
+
     /**
      * @var AttributeSetFactory
      */
     private $attributeSetFactory;
+
     /**
      * @param CustomerSetupFactory $customerSetupFactory
      * @param AttributeSetFactory $attributeSetFactory
@@ -28,41 +37,52 @@ class InstallData implements InstallDataInterface
         $this->customerSetupFactory = $customerSetupFactory;
         $this->attributeSetFactory = $attributeSetFactory;
     }
+
+
+    /**
+     * {@inheritdoc}
+     */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        /*customersetupfactory instead of eavsetupfactory */
+
+        /** @var CustomerSetup $customerSetup */
         $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
         $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
         $attributeSetId = $customerEntity->getDefaultAttributeSetId();
+
         /** @var $attributeSet AttributeSet */
         $attributeSet = $this->attributeSetFactory->create();
         $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
-        /* create customer selectline_id attribute */
-        $customerSetup->addAttribute(Customer::ENTITY,'selectline_id',	[
-                'type'         => 'varchar', // attribute with varchar type
-                'label'        => 'SelectLine ID',
-                'input'        => 'text',  // attribute input field is text
-                'required'     => false,  // field is not required
-                'visible'      => true,
-                'user_defined' => true,
-                'position'     => 999,
-                'sort_order'  => 999,
-                'system'       => 0,
-                'is_used_in_grid' => 1,   //setting grid options
-                'is_visible_in_grid' => 1,
-                'is_filterable_in_grid' => 1,
-                'is_searchable_in_grid' => 1,
-            ]
-        );
-        $sampleAttribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'selectline_id')
-            ->addData(
-                [
-                    'attribute_set_id' => $attributeSetId,
-                    'attribute_group_id' => $attributeGroupId,
-                    'used_in_forms' => ['adminhtml_customer','customer_account_edit','customer_account_create'],
+
+        $customerSetup->addAttribute(Customer::ENTITY, 'profile_picture', [
+            'type' => 'varchar',
+            'label' => 'Profile Picture',
+            'input' => 'image',
+            'backend' => 'Magelearn\CustomerAvatar\Model\Attribute\Backend\Avatar',
+            'required' => false,
+            'visible' => true,
+            'user_defined' => true,
+            'sort_order' => 10,
+            'position' => 10,
+            'system' => 0,
+            'is_used_in_grid' => true,
+            'is_visible_in_grid' => true,
+            'is_html_allowed_on_front' => true,
+            'visible_on_front' => true
+        ]);
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'profile_picture')
+            ->addData([
+                'attribute_set_id' => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms' => [
+                    'adminhtml_customer',
+                    'customer_account_create',
+                    'customer_account_edit'
                 ]
-// more used_in_forms ['adminhtml_checkout','adminhtml_customer','adminhtml_customer_address','customer_account_edit','customer_address_edit','customer_register_address']
-            );
-        $sampleAttribute->save();
+            ]);
+
+        $attribute->save();
     }
 }
